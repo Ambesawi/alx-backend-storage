63bca9241ca796ecab0d2f43936c43c59b84c117
Ambesawi <ambesawi77@gmail.com>
2023-10-11 20:42:35 +0000

    0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,10 @@
 -- create table users
 -- if not exists
 CREATE TABLE IF NOT EXISTS `users` (
-`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-`email` VARCHAR( 255 ) NOT NULL UNIQUE,
-`name` VARCHAR( 255 )
+`id` INT  NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`email` VARCHAR( 255 ) NOT NULL ,
+`name` VARCHAR( 255 ) ,
+UNIQUE (
+`email`
+)
 );
