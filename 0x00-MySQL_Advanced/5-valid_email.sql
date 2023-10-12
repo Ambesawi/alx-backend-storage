1e933dff932de0023439b783c3ef0553268e3ea7
Ambesawi <ambesawi77@gmail.com>
2023-10-12 19:05:30 +0000

    5-valid_email.sql

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- creates a trigger that resets the attribute valid_email
+-- only when the email has been changed
+DELIMITER $$ 
+CREATE TRIGGER email_trigger
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+IF NEW.email <> OLD.email
+THEN
+    SET NEW.valid_email = 0;
+END IF;
+END
+$$
+DELIMITER ;
