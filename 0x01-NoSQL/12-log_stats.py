36fdf6f10e9d572693ee6e46d47fda3e8db24aae
Ambesawi <ambesawi77@gmail.com>
2023-10-17 13:15:50 -0700

    12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python3
 """
-A script that provides some stats about Nginx logs stored in MongoDB
+script that provides some stats about Nginx logs stored in MongoDB
 """
 from pymongo import MongoClient
 
 
-def log_stats() -> None:
-    """
-    Provides some stats about Nginx logs stored in MongoDB
-    """
-    stats = ""
+if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
-    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    stats += "{} logs\nMethods:\n".format(nginx_collection.count_documents({}))
-    for m in method:
-        method_count = nginx_collection.count_documents({"method": m})
-        stats += '\tmethod {}: {}\n'.format(m, method_count)
-    stats += "{} status check".format(
-            nginx_collection.count_documents({"path": "/status"}))
-    print(stats)
-
-
-if __name__ == '__main__':
-    log_stats()
+    nginx_logs = client.logs.nginx
+    # get number of documents in collection
+    docs_num = nginx_logs.count_documents({})
+    get_num = nginx_logs.count_documents({'method': 'GET'})
+    post_num = nginx_logs.count_documents({'method': 'POST'})
+    put_num = nginx_logs.count_documents({'method': 'PUT'})
+    patch_num = nginx_logs.count_documents({'method': 'PATCH'})
+    delete_num = nginx_logs.count_documents({'method': 'DELETE'})
+    get_status = nginx_logs.count_documents({'method': 'GET',
+                                             'path': '/status'})
+    print("{} logs".format(docs_num))
+    print("Methods:")
+    print("\tmethod GET: {}".format(get_num))
+    print("\tmethod POST: {}".format(post_num))
+    print("\tmethod PUT: {}".format(put_num))
+    print("\tmethod PATCH: {}".format(patch_num))
+    print("\tmethod DELETE: {}".format(delete_num))
+    print("{} status check".format(get_status))
